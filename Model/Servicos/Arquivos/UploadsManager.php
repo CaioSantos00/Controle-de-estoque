7d9c8e925d97f5e13fec370682f4078c0de54de3
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2023-09-24 18:53:01 -0300

    boas práticas, tlgd

--- Model/Servicos/Arquivos/UploadsManager.php
+++ Model/Servicos/Arquivos/UploadsManager.php
@@ -5,21 +5,24 @@
 	
 	class UploadsManager{
 		static const $caminhoParaArvsSecund = "../../../ArquivosSecundarios"
-		static function salvarImagemDePerfilEnviada(string $idUsuario){
-			$caminhoParaDiretorio
-			$destino = self::"/FotosUsuarios/".$_FILES['fotoUsuario']['name'];
+		
+		static function salvarImagemDePerfilEnviada(string $idUsuario){		
+			$destino = self::$caminhoParaArvsSecund."/FotosUsuarios/";
 			
 			$imagem = Image::make($_FILES['fotoUsuario']['tmp_name']);
 			$imagem
-			->resize(300,300) //Redimensiona a imagem para um tamanho padrão
-			->encode("png", 70) //Define para uma extensão padrão e reduz a qualidade (para fins de compressão)
-			->save(); // Salvando na mesma instancia, alteramos a imagem original sem movê-la
+				->resize(300,300) //Redimensiona a imagem para um tamanho padrão
+				->encode("png", 70) //Define para uma extensão padrão e reduz a qualidade (para fins de compressão)
+				->save(); // Salvando na mesma instancia, alteramos a imagem original sem movê-la
 			
 			move_uploaded_file(
 				$_FILES['fotoUsuario']['tmp_name'],
-				$destino
+				$destino.$_FILES['fotosUsuarios']['tmp_name']
 			);
 			
-			rename($destino, )
+			rename(
+				$destino.$_FILES['fotoUsuario']['name'],
+				$destino.$idUsuario
+			);
 		}
 	}
