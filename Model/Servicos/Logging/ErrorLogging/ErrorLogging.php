884494a3f47846b636d1febfa910d9a951dec72c
felipeDosProgramas <111545218+felipeDosProgramas@users.noreply.github.com>
2023-09-27 07:15:11 -0300

    ok

---
+++ Model/Servicos/Logging/ErrorLogging/ErrorLogging.php
@@ -0,0 +1,40 @@
+<?php
+	namespace App\Servicos\Logging;	
+	
+	class LogDeErros{
+		const LOG_LOCATION = "ServerInfo/LogErros.txt";
+		const SEPARADOR_DE_ERROS = "<<<>>>";
+		public $logDeErros;
+		static bool $logInstanciado;
+		
+		function __construct(){
+			$this->setLogErros();			
+		}		
+		private function setLogErros(){
+			if(empty($this->logDeErros)){
+				$this->logDeErros = fopen(self::LOG_LOCATION, 'a');
+				//var_dump(scandir("./"));
+				self::$logInstanciado = true;				
+				if(!$this->logDeErros) self::$logInstanciado = false;
+			}
+		}
+		function criarLinhaDeErro(string $onde, string $mensagem) :string{
+			$linhaDeErro = array(
+			"quando"	=> date('d-m-y \a\s h:i',strtotime('now')),
+			"onde"		=> $onde,
+			"mensagem" 	=> $mensagem
+			);
+			return self::SEPARADOR_DE_ERROS.json_encode($linhaDeErro)."\n";
+		}
+		function setErro(string $onde, string $mensagem){			
+			if(self::$logInstanciado) fwrite($this->logDeErros, $this->criarLinhaDeErro($onde, $mensagem));			
+		}
+
+		function __destruct(){
+			if(self::$logInstanciado){
+				fclose($this->logDeErros);
+				self::$logInstanciado = false;			
+			}
+		}
+	}
+	
