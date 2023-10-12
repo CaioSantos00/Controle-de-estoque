d24b00ebb003bb20ed3fcd60042141410fb0865f
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-10-12 13:02:33 -0300

    Criação de Variações

--- View/RecursosEstaticos/js/cadasterProductCreation/creationElements.js
+++ View/RecursosEstaticos/js/cadasterProductCreation/creationElements.js
@@ -1,6 +1,7 @@
 let btnCriaClassi = document.getElementById('criaClassi')
 let divHoldClassifi = document.getElementById('divHoldClassifi')
 let divHoldVari = document.getElementById('divHoldVari')
+let criaVari = document.getElementById('criaVari')
 
 function criaClassificacao() {
     let cardsClassificacoes = document.createElement('div')
@@ -9,7 +10,7 @@ function criaClassificacao() {
     let inputsClassi = document.createElement('input')
     inputsClassi.classList.add('inputs')
     inputsClassi.type = 'text'
-    //inputsClassi.name = ''
+    inputsClassi.name = ''
 
     let btnsCancel = document.createElement('button')
     btnsCancel.classList.add('btnsCancel')
@@ -24,4 +25,44 @@ function criaClassificacao() {
 
 btnCriaClassi.addEventListener('click', () => {
     criaClassificacao()
+})
+
+function criaVariacao() {
+    let cardsVariacoes = document.createElement('div')
+    cardsVariacoes.classList.add('cardsVariacoes')
+
+    let holdInputs = document.createElement('div')
+    holdInputs.classList.add('holdInputs')
+    let inputQtd = document.createElement('input')
+    inputQtd.classList.add('inputVari')
+    inputQtd.type = 'number'
+    inputQtd.min = 1
+    inputQtd.name = ''
+    let inputPreco = document.createElement('input')
+    inputPreco.type = ''
+    inputPreco.name = ''
+    inputPreco.classList.add('inputVari')
+    let btnsCancel = document.createElement('button')
+    btnsCancel.classList.add('btnsCancel')
+    btnsCancel.innerText = 'Cancelar'
+    holdInputs.append(inputQtd, inputPreco, btnsCancel)
+
+    let divTextAreaFile = document.createElement('div')
+    divTextAreaFile.classList.add('divTextAreaFile')
+    let variacoesTextArea = document.createElement('textarea')
+    variacoesTextArea.classList.add('inputs', 'variacoesTextArea')
+    let inputFile = document.createElement('input')
+    inputFile.classList.add('inputFile', 'inputs')
+    inputFile.type = 'file'
+    inputFile.name = ''
+    let buttonConfirm = document.createElement('button')
+    buttonConfirm.classList.add('btnsConfirm')
+    buttonConfirm.innerText = 'Salvar'
+    divTextAreaFile.append(variacoesTextArea, inputFile, buttonConfirm)
+    cardsVariacoes.append(holdInputs, divTextAreaFile)
+    divHoldVari.appendChild(cardsVariacoes)
+}
+
+criaVari.addEventListener('click', () => {
+    criaVariacao()
 })
