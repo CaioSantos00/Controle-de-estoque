a3abd091211ee6a75d3538e245f416fca941ca8e
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2023-10-12 17:20:46 -0300

    Revert "Ve oq ce acha 👍"
    This reverts commit 98c6cc55328081edd4e6fdcc30c259911a17589b.

--- View/RecursosEstaticos/js/cadasterProductCreation/creationElements.js
+++ View/RecursosEstaticos/js/cadasterProductCreation/creationElements.js
@@ -2,40 +2,26 @@ let btnCriaClassi = document.getElementById('criaClassi')
 let divHoldClassifi = document.getElementById('divHoldClassifi')
 let divHoldVari = document.getElementById('divHoldVari')
 
-function criarDiv(){
+function criaClassificacao() {
     let cardsClassificacoes = document.createElement('div')
     cardsClassificacoes.classList.add('cardsClassificacoes')
-    return cardsClassificacoes;
-}
-function criarInput(){
-    let inputsClassi = document.createElement('input');
-    inputsClassi.classList.add('inputs');
-    inputsClassi.type = 'text';
 
-    return inputsClassi;
-}
-function criarBotaoCancelar(){
-    let btnsCancel = document.createElement('button');
-    btnsCancel.classList.add('btnsCancel');
-    btnsCancel.innerText = 'Cancelar';   
+    let inputsClassi = document.createElement('input')
+    inputsClassi.classList.add('inputs')
+    inputsClassi.type = 'text'
+    //inputsClassi.name = ''
 
-    return btnsCancel;
-}
-function criarBotaoConfirmar(){
-    let btnsConfirm = document.createElement('button');
-    btnsConfirm.classList.add('btnsConfirm');
-    btnsConfirm.innerText = 'Cancelar';
-    return btnsConfirm;
-}
-function criaClassificacao() {
-    let cardsClassificacoes = criarDiv(),
-        inputsClassi = criarInput(),
-        btnsCancel = criarBotaoCancelar(), 
-        btnsConfirm = criarBotaoConfirmar();
-    cardsClassificacoes.append(inputsClassi, btnsCancel, btnsConfirm);
-    return cardsClassificacoes;        
+    let btnsCancel = document.createElement('button')
+    btnsCancel.classList.add('btnsCancel')
+    btnsCancel.innerText = 'Cancelar'
+
+    let btnsConfirm = document.createElement('button')
+    btnsConfirm.classList.add('btnsConfirm')
+    btnsConfirm.innerText = 'Cancelar'
+    cardsClassificacoes.append(inputsClassi, btnsCancel, btnsConfirm)
+    divHoldClassifi.appendChild(cardsClassificacoes)
 }
 
 btnCriaClassi.addEventListener('click', () => {
-    divHoldClassifi.appendChild(criaClassificacao());
+    criaClassificacao()
 })
