b2eabb9805ab632b30e27b6e76725a8f5e80edde
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-10-26 14:19:16 -0300

    Tela de finalizar pedido

---
+++ View/js/Requisicoes/requisicaoCarrinho/endereco.js
@@ -0,0 +1,28 @@
+const nomeEndereco = document.getElementById('nomeEndereco'),
+      telEndereco = document.getElementById('telEndereco'),
+      ruaEndereco = document.getElementById('ruaEndereco'),
+      numberEndereco = document.getElementById('numberEndereco'),
+      bairroEndereco = document.getElementById('bairroEndereco'),
+      estadoEndereco = document.getElementById('estadoEndereco'),
+      cityEndereco = document.getElementById('cityEndereco')
+
+async function loadEndereco() {
+    const resposta = await fetch('http://localhost/Sites/Repositorios/tcc/Sistema-de-pedidos-TCC/View/js/jsonEndereco.json')
+    if (!resposta.ok) {
+        console.log("Erro de Solicitação !")
+    }
+    try {
+    const respostaBusca = await resposta.json()
+    nomeEndereco.innerText = respostaBusca.nome
+    telEndereco.innerText = respostaBusca.telefone
+    ruaEndereco.innerText = respostaBusca.endereco.rua
+    numberEndereco.innerText = respostaBusca.endereco.numero
+    bairroEndereco.innerText = respostaBusca.endereco.bairro
+    estadoEndereco.innerText = respostaBusca.endereco.estado
+    cityEndereco.innerText = respostaBusca.endereco.cidade
+    } catch {
+        console.log('Erro')
+    }
+}
+
+loadEndereco()
--- View/js/elementos/header.js
+++ View/js/elementos/header.js
@@ -1,5 +1,5 @@
 async function loadHeader() {
-    const resposta = await fetch('http://localhost/Sites/Repositorios/Sistema-de-pedidos-TCC/View/components/header.html')
+    const resposta = await fetch('http://localhost/Sites/Repositorios/tcc/Sistema-de-pedidos-TCC/View/components/header.html')
     const headerText = await resposta.text()
     const headerContainer = document.querySelector('.header-container');
      headerContainer.innerHTML = headerText;
