ac161522f71ace75e9199b43896ed072749456b7
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2023-10-30 23:33:41 -0300

    consulta de endereco

--- Model/Enderecos/Consultar.php
+++ Model/Enderecos/Consultar.php
@@ -1,6 +1,41 @@
 <?php
 	namespace App\Enderecos;
 
-	use
+    use App\Servicos\Conexao\ConexaoBanco as CB;
+    use App\Interfaces\Model;
 
+    class Consultar implements Model{
+        private string $idUsuario;
+        private array $resultadoConsulta;        
+        private string $query = "select 
+            `Id`,`nomeEndereco`,`Cep`,`Cidade`,`Rua`,`Bairro`,`Numero`,
+            `DataCriacao`,`InstrucoesEntrega`,`dataModificacao` 
+            from `enderecos` where `IdDono` = ?";
+        function __construct(string $idUsuario){
+            $this->idUsuario = $idUsuario;
+        }
+
+        private function consultarBanco() :array|string{            
+            try{
+                $resultado = [];
+                CB::getConexao()->beginTransaction();
+                $query = CB::getConexao()->prepare($this->query);
+                $query->execute([$this->idUsuario]);
+                $resultado = $query->fetchAll();
+                CB::getConexao()->commit();
+            }
+            catch(\PDOException|\Exception $ex){
+                $GLOBALS['ERRO']->setErro("consulta de endereco", $ex->getMessage());
+                CB::voltaTudo();
+                $resultado = [];
+            }              
+            finally{
+                return $resultado;
+            }
+        }
+        function getResposta(){
+            if(empty($this->resultadoConsulta)) $this->resultadoConsulta = $this->consultarBanco();
+            return $this->resultadoConsulta;
+        }
+    }
  
--- Testes/enderecos/ConsultarTest.php
+++ Testes/enderecos/ConsultarTest.php
@@ -1,2 +1,16 @@
 <?php
-	namespace 
+    namespace Testes\enderecos;
+    
+    require_once "vendor/autoload.php";
+
+    use App\Enderecos\Consultar;
+
+    class ConsultarTest extends \PHPUnit\Framework\TestCase{
+        private string $idUsuario = "36";
+        private string $resultadoEsperado = '[{"Id":28,"0":28,"nomeEndereco":"casa","1":"casa","Cep":"11740-000","2":"11740-000","Cidade":"Itanha\u00e9m","3":"Itanha\u00e9m","Rua":"av lydia","4":"av lydia","Bairro":"Loty","5":"Loty","Numero":"1160","6":"1160","DataCriacao":"29.10.23  11:46","7":"29.10.23  11:46","InstrucoesEntrega":"deixar no vizinho e na rua de baixo","8":"deixar no vizinho e na rua de baixo","dataModificacao":"29.10.23  11:46","9":"29.10.23  11:46"}]';
+        function testConsultaNormal(){
+            $consulta = new Consultar($this->idUsuario);
+            $this->assertIsArray($consulta->getResposta());
+            $this->assertEquals($this->resultadoEsperado , json_encode($consulta->getResposta()));
+        }
+    }
