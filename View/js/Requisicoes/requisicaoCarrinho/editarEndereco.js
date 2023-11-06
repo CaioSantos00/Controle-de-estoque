2f704ea5124ab49ac35d6417cb2b6b3268934f2f
Caio dos Santos Oliveira <caiossantos4321@gmail.com>
2023-11-06 15:41:03 -0300

    Edit

--- View/js/Requisicoes/requisicaoCarrinho/editarEndereco.js
+++ View/js/Requisicoes/requisicaoCarrinho/editarEndereco.js
@@ -8,31 +8,34 @@ let nomeCompleto = document.getElementById('nomeCompleto'),
     cep = document.getElementById('cep'),
     rua = document.getElementById('rua'),
     numero = document.getElementById('numero'),
-    btnSalvarEndere = document.getElementById('btnSalvarEndere')
+    btnSalvarEndere = document.getElementById('btnSalvarEndere');
 
-
-async function editEndereco() {
-    const resposta = await fetch('http://localhost/Sites/Repositorios/tcc/Sistema-de-pedidos-TCC/View/js/jsonEndereco.json')
-    if (!resposta.ok) {
-        console.log("Erro de Solicitação !")
-    }
+function enviaDadosFront(respostaBusca){
     try {
-    const respostaBusca = await resposta.json()
-    nomeCompleto.value = respostaBusca.nome
-    telefone.value = respostaBusca.telefone
-    rua.value = respostaBusca.endereco.rua
-    numero.value = respostaBusca.endereco.numero
-    bairro.value = respostaBusca.endereco.bairro
-    estado.value = respostaBusca.endereco.estado
-    cidade.value = respostaBusca.endereco.cidade
-    cep.value = respostaBusca.endereco.cep
-    } catch (errorQual) {
-        console.log('Erro' + errorQual)
-    }
+        respostaBusca = JSON.parse(respostaBusca)
+        nomeCompleto.value = respostaBusca.nome
+        telefone.value = respostaBusca.telefone
+        rua.value = respostaBusca.endereco.rua
+        numero.value = respostaBusca.endereco.numero
+        bairro.value = respostaBusca.endereco.bairro
+        estado.value = respostaBusca.endereco.estado
+        cidade.value = respostaBusca.endereco.cidade
+        cep.value = respostaBusca.endereco.cep
+        } catch (errorQual) {
+            console.log('Erro' + errorQual)
+        }    
 }
+if(sessionStorage.getItem("requisicao")){
+     enviaDadosFront(sessionStorage.getItem("requisicao"))
+}else{
+    (async ()=> {
 
-btnSalvarEndere.addEventListener('click', () => {
-   
-})
-
-editEndereco()
+        const resposta = await fetch('http://localhost/Sites/Repositorios/tcc/Sistema-de-pedidos-TCC/View/js/jsonEndereco.json')
+        const respostaBusca = await resposta.text()
+        if (!resposta.ok) {
+            console.log("Erro de Solicitação na API!")
+            return
+        }        
+    enviaDadosFront(respostaBusca)
+})(btnSalvarEndere)
+}
--- View/js/Requisicoes/requisicaoCarrinho/endereco.js
+++ View/js/Requisicoes/requisicaoCarrinho/endereco.js
@@ -4,27 +4,40 @@ const nomeEndereco = document.getElementById('nomeEndereco'),
       numberEndereco = document.getElementById('numberEndereco'),
       bairroEndereco = document.getElementById('bairroEndereco'),
       estadoEndereco = document.getElementById('estadoEndereco'),
-      cityEndereco = document.getElementById('cityEndereco')
+      cityEndereco = document.getElementById('cityEndereco'),
+      aEdit = document.getElementById('aEdit')
 
-async function loadEndereco() {
+      aEdit.addEventListener('click', ()=>{
+        
+      })
+      
+      function enviaDadosFront(respostaBusca){
+          try{
+              respostaBusca = JSON.parse(respostaBusca)
+              sessionStorage.setItem("requisicao", respostaBusca.nome);
+        nomeEndereco.innerText = respostaBusca.nome
+        telEndereco.innerText = respostaBusca.telefone
+        ruaEndereco.innerText = respostaBusca.endereco.rua
+        numberEndereco.innerText = respostaBusca.endereco.numero
+        bairroEndereco.innerText = respostaBusca.endereco.bairro
+        estadoEndereco.innerText = respostaBusca.endereco.estado
+        cityEndereco.innerText = respostaBusca.endereco.cidade
+    }
+    catch(e){
+        console.log(e)
+    }    
+}
+(async ()=> {
     const resposta = await fetch('http://localhost/Sites/Repositorios/tcc/Sistema-de-pedidos-TCC/View/js/jsonEndereco.json')
     if (!resposta.ok) {
-        console.log("Erro de Solicitação !")
-    }
+        console.log("Erro de Solicitação !");
+        return;
+    }        
     try {
-    const respostaBusca = await resposta.json()
-    nomeEndereco.innerText = respostaBusca.nome
-    telEndereco.innerText = respostaBusca.telefone
-    ruaEndereco.innerText = respostaBusca.endereco.rua
-    numberEndereco.innerText = respostaBusca.endereco.numero
-    bairroEndereco.innerText = respostaBusca.endereco.bairro
-    estadoEndereco.innerText = respostaBusca.endereco.estado
-    cityEndereco.innerText = respostaBusca.endereco.cidade
-
-    //localStorage.setItem("nome", respostaBusca.nome)
+    const respostaBusca = await resposta.text();
+    sessionStorage.setItem("requisicao", respostaBusca);
+    if(sessionStorage.getItem("requisicao")) enviaDadosFront(respostaBusca);    
     } catch (qualErro){
         console.log('Erro' + qualErro)
     }
-}
-
-loadEndereco()
+})()
