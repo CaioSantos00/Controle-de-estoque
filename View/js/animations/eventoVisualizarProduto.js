903aeff69db227504ceb3a136c5b27102f36571b
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-11-11 18:55:35 -0300

    Carrosel imagens

--- View/js/animations/eventoVisualizarProduto.js
+++ View/js/animations/eventoVisualizarProduto.js
@@ -1,6 +1,9 @@
 let divDescription = document.getElementById('divDescription'),
     descriptionProduct = document.getElementById('descriptionProduct'),
-    descriptionProductFalseTrue = false
+    descriptionProductFalseTrue = false,
+    btnEsquerda = document.getElementById('btnEsquerda'),
+    btnDireita = document.getElementById('btnDireita'),
+    mudaImg = document.getElementById('mudaImg')
  
 divDescription.addEventListener('click', () => {
     descriptionProductFalseTrue = !descriptionProductFalseTrue
@@ -10,3 +13,23 @@ divDescription.addEventListener('click', () => {
     }
 })
 
+/* Carrosel de imagens */
+
+let arrayImgs = ['../imgs/amortece.webp', '../imgs/amortecedor.jpg', '../imgs/amortecedor2.jpg']
+let i = 0
+btnDireita.addEventListener('click', ()=> {
+    mudaImg.src = arrayImgs[i++]
+    if (arrayImgs.length === i) {
+        i = 0
+    }
+})
+
+btnEsquerda.addEventListener('click', ()=> {
+    if (i == 0) {
+       i = arrayImgs.length - 1
+       console.log(i)
+    } else {
+        i--
+    }
+    mudaImg.src = arrayImgs[i]
+})
