67e32f7d61f4739fb26c250471748085964bbdba
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-11-13 20:36:11 -0300

    Menu responsivo

--- View/js/elementos/header.js
+++ View/js/elementos/header.js
@@ -3,6 +3,14 @@ async function loadHeader() {
     const headerText = await resposta.text()
     const headerContainer = document.querySelector('.header-container');
      headerContainer.innerHTML = headerText;
+     let menuHam = document.getElementById('menuHam')
+let nav = document.querySelector('nav')
+
+console.log(menuHam)
+menuHam.addEventListener('click', ()=> {
+    nav.classList.toggle('nav-response')
+})
 }
+
 
 loadHeader()
