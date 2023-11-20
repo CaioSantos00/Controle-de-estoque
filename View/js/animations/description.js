1bf8cb4e2f85481cb25d8b9c62e1a7dfef1461dd
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-11-19 21:31:57 -0300

    Tela de Detalhes das Mensagens

---
+++ View/js/animations/description.js
@@ -0,0 +1,11 @@
+let divDescription = document.getElementById('divDescription'),
+    descriptionProduct = document.getElementById('descriptionProduct'),
+    descriptionProductFalseTrue = false
+ 
+divDescription.addEventListener('click', () => {
+    descriptionProductFalseTrue = !descriptionProductFalseTrue
+    descriptionProduct.style.display = 'flex'
+    if (!descriptionProductFalseTrue) {
+        descriptionProduct.style.display = 'none'
+    }
+})
