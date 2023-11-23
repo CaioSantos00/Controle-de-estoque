0c0384951cf4520e2d9d1bac667556eea9dda6c3
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-11-23 10:17:14 -0300

    Tela de Cadastrar Produto
    Está dando erro na linha 188, está falando que é sintaxe, mas dei uma olhada e aparenta estar certinho

--- View/js/cadasterProductCreation/creationElements.js
+++ View/js/cadasterProductCreation/creationElements.js
@@ -5,11 +5,11 @@ let criaVari = document.getElementById('criaVari')
 let selectClassi = document.getElementById('selectClassi')
 
 
-function criaBtnCancel(divPai ,divQualApagar) {
+function criaBtnCancel(divPai, divQualApagar) {
     let btnsCancel = document.createElement('button')
     btnsCancel.classList.add('btnsCancel')
     btnsCancel.innerText = 'Cancelar'
-    btnsCancel.onclick = () => btnCancel(divPai ,divQualApagar)
+    btnsCancel.onclick = () => btnCancel(divPai, divQualApagar)
     return btnsCancel
 }
 
@@ -26,7 +26,7 @@ function criaClassificacao() {
     inputsClassi.type = 'text'
     inputsClassi.name = ''
     inputsClassi.maxLength = 25
-    inputsClassi.addEventListener('input', ()=>{
+    inputsClassi.addEventListener('input', () => {
         if (inputsClassi.value.length >= 25) alert('Máximo de 30 caracteres')
     })
 
@@ -38,20 +38,25 @@ function criaClassificacao() {
     btnsConfirm.onclick = async () => {
         let limpaStr = inputsClassi.value.trim()
         if (limpaStr == "") alert('Digite o valor')
-        if (limpaStr) {
-        const urlBusca = await fetch('/envio/cadastrarClassificacao', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({"nome": limpaStr})
-        }) 
-        alert('Cadastrado')
-        selectClassi.append(criaOption(limpaStr))
-        cardsClassificacoes.remove()
-        }
+        try {
+            const urlBusca = await fetch('/envio/cadastrarClassificacao', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({"nome": limpaStr})
+            });
 
-    }
+            if (!urlBusca.ok) {
+                console.log("Erro na solicitação" + urlBusca.status);
+            } else {
+                alert('Cadastrado');
+                selectClassi.append(criaOption(limpaStr));
+                cardsClassificacoes.remove();
+            }
+        } catch (error) {
+            console.log(error);
+        }
 
     cardsClassificacoes.append(inputsClassi, btnsCancel, btnsConfirm)
     divHoldClassifi.appendChild(cardsClassificacoes)
@@ -92,11 +97,11 @@ function criaVariacao() {
     inputFile.classList.add('inputFile', 'inputs')
     inputFile.type = 'file'
     inputFile.name = ''
-    
+
     let buttonConfirm = document.createElement('button')
     buttonConfirm.classList.add('btnsConfirm')
     buttonConfirm.innerText = 'Salvar'
-    buttonConfirm.onclick = () => {}
+    buttonConfirm.onclick = () => { }
 
     divTextAreaFile.append(variacoesTextArea, inputFile, buttonConfirm)
     cardsVariacoes.append(holdInputs, divTextAreaFile)
@@ -107,46 +112,46 @@ criaVari.addEventListener('click', () => {
     criaVariacao()
 });
 
-async function consultarClassificacoes(){
+async function consultarClassificacoes() {
     let classificacoes = await fetch("/envio/consultarClassificacoes")
     return await classificacoes.text()
 }
-async function salvarClassificacao(){
+async function salvarClassificacao() {
 
 }
-function criaOption(valor){
+function criaOption(valor) {
     let options = document.createElement('option');
-        options.value = valor;
-        options.innerText = valor;
+    options.value = valor;
+    options.innerText = valor;
     return options;
 }
-async function excluiClassi(classi){
-    classi = classi.trim();    
-    if(classi == "") return false;
+async function excluiClassi(classi) {
+    classi = classi.trim();
+    if (classi == "") return false;
     let serv = await fetch("/envio/excluirClassificacao", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({"paraExcluir": classi})
+        body: JSON.stringify({ "paraExcluir": classi })
     });
     let resp = await serv.text() == "tudo certo"
         ? true
         : false;
-    if(resp){
+    if (resp) {
         selectClassi.innerText = "";
         let opcoes = await consultarClassificacoes();
         opcoes = JSON.parse(opcoes);
         opcoes.forEach((e) => {
-            selectClassi.append(criaOption(e))     
+            selectClassi.append(criaOption(e))
         });
     }
 }
-async function editarClassificacao(antigo, novo){    
-        antigo = antigo.trim()
-        novo = novo.trim()
-        if(novo == "" || antigo == "") return false;
-        let serv = await fetch("/envio/editarClassificacao", {
+async function editarClassificacao(antigo, novo) {
+    antigo = antigo.trim()
+    novo = novo.trim()
+    if (novo == "" || antigo == "") return false;
+    let serv = await fetch("/envio/editarClassificacao", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -159,25 +164,25 @@ async function editarClassificacao(antigo, novo){
     let resp = await serv.text() == "tudo certo"
         ? true
         : false;
-    if(resp){
+    if (resp) {
         selectClassi.innerText = "";
         let opcoes = await consultarClassificacoes();
         opcoes = JSON.parse(opcoes);
         opcoes.forEach((e) => {
-            selectClassi.append(criaOption(e))     
+            selectClassi.append(criaOption(e))
         });
     }
 }
 (async () => {
-  try{
-    let opcoes = await consultarClassificacoes();
+    try {
+        let opcoes = await consultarClassificacoes();
         opcoes = JSON.parse(opcoes);
         opcoes.forEach((e) => {
-            selectClassi.append(criaOption(e))     
+            selectClassi.append(criaOption(e))
         });
-    console.log(opcoes)
-  }
-  catch(e){
-    console.log(e)
-  }
+        console.log(opcoes)
+    }
+    catch (e) {
+        console.log(e)
+    }
 })()
