02cf66b94b40afddcd6e98631ed36d3a41bb9d17
felipeDosProgramas <felipeluizmsouza@gmail.com>
2023-11-23 10:12:06 -0300

    correção de endpoint

--- Controller/Rotas/RotasUser/UserRequests/UserRequests.php
+++ Controller/Rotas/RotasUser/UserRequests/UserRequests.php
@@ -11,6 +11,7 @@
 	class UserRequests{
 		function __construct(){
 			if(!isset($_POST['Submit'])) exit("Bela tentativa, hacker...");
+			if(count($_POST) > 0) array_map('trim',$_POST);
 		}
 		private function verificarExiste(string|array $dados) :bool{
 			if(is_array($dados)){
@@ -23,32 +24,30 @@
 			return true;
 		}
 		function cadastro($data) :void{
+			if(!$this->verificarExiste([$_POST['Nome'],$_POST['Email'],$_POST['Telefone'],$_POST['Senha']])) exit("Bela tentativa, hacker...");
 			$senha = password_hash($_POST['Senha'], PASSWORD_DEFAULT);
 			if(is_bool($senha)) exit("erro interno");
 			$cadastro = new User;
-			$dadosUsuario = [
-				$_POST['Nome'],
-				$senha,
-				$_POST['Email'],
-				$_POST['Telefone'],
-				0
-			];
+			$dadosUsuario = [$_POST['Nome'],$senha,$_POST['Email'],$_POST['Telefone'],0];
 			$cadastro->setDadosUsuario($dadosUsuario);
 			echo $cadastro->getResposta();
 		}
 		function login($data) :void{
-			if(!isset($_POST['Email']) or !isset($_POST['Senha'])) exit("Bela tentativa, hacker...");
+			if(!$this->verificarExiste([$_POST['Email'],$_POST['Senha']])) exit("Bela tentativa, hacker...");
 			$usuario = new Login($_POST['Email'], $_POST['Senha']);
 			echo $usuario->getResposta();
 		}
 		function perfil($data) :void{
+			if(!$this->verificarExiste($_COOKIE['login'])) exit("não esta logado");
 			echo new Perfil($_COOKIE['login']);
 		}
 		function excluirPerfil($data){
-			if(!isset($_POST['idUsuario']) or $_POST['idUsuario'] != $_COOKIE['login'])
-			   	exit("Bela tentativa, hacker...");
-			$perfil = new EP($_POST['idUsuario']);
-			echo $perfil->executar();
+			if($this->verificarExiste([$_POST['idUsuario'],$_COOKIE['login']]) and $_POST['idUsuario'] == hex2bin($_COOKIE['login'])){
+				$perfil = new EP($_POST['idUsuario']);
+				echo $perfil->executar();
+				return;
+			}
+			exit("Bela tentativa, hacker...");
 		}
 		function consultaGeral($data):void {
 			echo new CG;
