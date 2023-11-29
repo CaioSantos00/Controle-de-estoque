1de0673924a2273366922691a16a5afce9d5e1a1
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-11-29 14:39:09 -0300

    Carregamento etc...

--- View/js/envio/cadastroUsuario.js
+++ View/js/envio/cadastroUsuario.js
@@ -7,16 +7,30 @@ let Envio = new EnvioDadosUsuario(formulario, btnEnvio, "cadastro");
 	Envio.xhr.addEventListener("progress", () => {
 		qualMensagem.style.display = 'flex'
 		qualMensagem.src = '/estaticos/imgs/loader.gif'
-		qualMensagem.style.display = 'none'
 		console.log("adicionar rodinha girando dizendo que ta carregando/enviando sla");
 	})
 	Envio.xhr.addEventListener("load", () => {
 		//esteja preparado para fazer uma série de avisos a partir das
 		//possíveis respostas vindas do back
-		console.log(Envio.xhr.responseText)
+		console.log(Envio.xhr.responseText)		
+		formulario.style.display = 'none'
+		qualMensagem.src = '/estaticos/imgs/correct.png'
+		let linkLogar = document.createElement('a')
+		linkLogar.innerText = 'Clique aqui para fazer login'
+		linkLogar.href = '/login'
+		linkLogar.id = 'aSetinhaBoa'
+		mensagensCadaster.appendChild(linkLogar)
+
 		console.log("adicionar setinha verdinha dizendo 'cadastrou certin, confrade' ou coisa do tipo");			
 	})
 	Envio.xhr.addEventListener("error", () => {
+		formulario.style.display = 'none'
+		qualMensagem.src = '/estaticos/imgs/incorrect.png'
+		let linkLogar = document.createElement('a')
+		linkLogar.innerText = 'Clique aqui e tente novamente'
+		linkLogar.href = '/cadastro'
+		linkLogar.id = 'aSetinhaRuim'
+		mensagensCadaster.appendChild(linkLogar)
 		console.log("adicionar Xis vermelho dizendo 'deu errado meu nobre'");
 	})
 	console.log("sdsds");
