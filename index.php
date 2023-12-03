3163c3ca7dbbb6d29ae41134c0ac958b31c3e2d6
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2023-12-02 21:37:15 -0300

    altas explicações brodinho

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
 	//------------------------------------------------------------------------------------------------------------------------------------
 	$router->namespace("Controladores\Rotas");
 	$router->group("estaticos");
-	$router->get('/fotoPerfilUser/{idUser}',"RotasSecundarias:fotoUsuario");
+	$router->get('/fotoPerfilUser/{idUser}',"RotasSecundarias:fotoUsuario"); // ROTA PARA RECUPERAR IMAGENS DE PERFIL DE UM USUARIO, É PELO ID DELE
 	$router->get('/estilo', "RotasEstaticas:estilos");
 	$router->get('/componentes/{nome}',"RotasEstaticas:elementos");
-	$router->get('/imgs/{qual}', "RotasSecundarias:img");
-	$router->get('/imgs/mensagem/{idMsg}/{nomeFoto}',"RotasSecundarias:imgMsg");
+	$router->get('/imgs/{qual}', "RotasSecundarias:img"); //ESSA AQ CE JÁ SABE
+	$router->get('/imgs/mensagem/{idMsg}/{nomeFoto}',"RotasSecundarias:imgMsg"); // ESSA AQ É PRA RECUPERAR IMAGENS DE UMA MENSAGEM, TENQ TER O ID DA MSG E O NOME DA IMAGEM
 	$router->get('/js/{contexto}/{nome}', "RotasEstaticas:script");
 	$router->get('/js/subdir/{contexto}/{nomeSubDir}/{nomesArquivosSeparadosPorVirgula}',"RotasEstaticas:subdir");
 	$router->get('/js/modulos/{contexto}/{scriptPrincipal}/{nomesDosModulosSeparadosPorVirgula}',"RotasEstaticas:scriptModularizado");
@@ -41,12 +41,14 @@
 	$router->post("/login", "UserRequests:login");
 	$router->post("/cadastro", "UserRequests:cadastro");
 	$router->post("/mensagem", "MensagensRequests:enviarMensagem");
-	$router->get('/minhasMensagens',"MensagensRequests:consultarMensagens");
-	
+	$router->get('/minhasMensagens',"MensagensRequests:consultarMensagens"); // DAQUI CE VAI RECUPERAR DADOS DAS MENSAGENS DO USUARIO QUE TIVER LOGADO NO MOMENTO	
 	$router->get('/finalizadosUsuarioEspecifico',"CarrinhoRequests:finalizados");	
-	$router->get('/perfil',"UserRequests:perfil");
-	$router->get('/logoff', "UserRequests:deslogar");
+	$router->get('/perfil',"UserRequests:perfil"); // DAQUI CE VAI RECUPARAR DADOS DO USUARIO LOGADO NO MOMENTO
+	$router->get('/logoff', "UserRequests:deslogar"); // DAQUI CE VAI DESLOGAR O USUARIO QUE TIVER LOGADO
 	$router->get('/minhaMensagem/{idMsg}/{arqvs}',"MensagensRequests:consultarMensagemEspecifica");
+	// DAQUI CE VAI CONSULTAR UMA MENSAGEM ESPECIFICA, ESSA É PRAS TELAS DO USUARIO.
+	//pra buscar os arquivos da mensagem tenq passar um sim no final da requisição. se quizer dps explica,
+	//mas vai lendo a função MensagensRequests:consultarMensagemEspecifica
 /*--------------------------------------------------------------------------------------------------------------------------------------------*/
 	$router->namespace("Controladores\Rotas\RotasAdm");
 	$router->group("admin");
