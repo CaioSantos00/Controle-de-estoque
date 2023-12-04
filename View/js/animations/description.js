2495101c89fd2f88489cb1cf4f9fdc005ae78c60
Caio dos Santos Oliveira <96753850+CaioSantos00@users.noreply.github.com>
2023-12-04 20:48:05 -0300

    Variação

--- View/js/animations/description.js
+++ View/js/animations/description.js
@@ -1,11 +1,34 @@
 let divDescription = document.getElementById('divDescription'),
     descriptionProduct = document.getElementById('descriptionProduct'),
-    descriptionProductFalseTrue = false
+    descriptionProductFalseTrue = false,
+    divChecks = document.getElementById('divChecks'),
+    selectClassifi = document.getElementById('selectClassifi'),
+    selectVaria = document.getElementById('selectVaria'),
+    divChecksVar = document.getElementById('divChecksVar')
  
-divDescription.addEventListener('click', () => {
+/*divDescription.addEventListener('click', () => {
     descriptionProductFalseTrue = !descriptionProductFalseTrue
     descriptionProduct.style.display = 'flex'
     if (!descriptionProductFalseTrue) {
         descriptionProduct.style.display = 'none'
     }
-})
+})*/
+
+function abreEfecha(divPai, divEscondida) {
+    divPai.addEventListener('click', () => {
+        descriptionProductFalseTrue = !descriptionProductFalseTrue
+        divEscondida.style.display = 'block'
+        if (!descriptionProductFalseTrue) {
+            divEscondida.style.display = 'none'
+        }
+    })
+}
+
+if (divDescription && descriptionProduct) {
+    abreEfecha(divDescription, descriptionProduct)
+} else {
+    console.log('Elemento não existe')
+}
+
+abreEfecha(selectClassifi, divChecks)
+abreEfecha(selectVaria, divChecksVar)
